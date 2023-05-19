ba15f8a50114cf872abd44b18e8d4c7335c09ab3
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-05-19 14:19:04 -0500

    Add: Error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import authRoutes from './routes/auth.routes.js';
+import { notFound, errorHandler } from './middlewares/error.middleware.js';
 import { PORT, ENV } from './config/secrets.js';
 
 const app = express();
@@ -14,6 +15,9 @@ app.use('/api/v1/auth', authRoutes);
 app.get('/', (req, res) => {
   res.send('Hello');
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port: ${PORT} in ${ENV} mode`);
