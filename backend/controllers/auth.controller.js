f738e74e51b92e40af20f1856c9ead2c1dce6dbb
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-05-19 14:08:49 -0500

    Add: express async handler

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,15 @@
+import asyncHandler from 'express-async-handler';
+
 // @desc    Login User / set token
 // @route   POST /api/v1/auth/login
 // @access  Public
-export const login = (req, res) => {
+export const login = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Hello User' });
-};
+});
 
 // @desc    Register User / set token
 // @route   POST /api/v1/auth/register
 // @access  Public
-export const register = (req, res) => {
+export const register = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Hello New User' });
-};
+});
