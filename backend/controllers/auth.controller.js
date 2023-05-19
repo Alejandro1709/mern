7cbc732b4811e4d6701f5693c9869bf4de9d9669
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-05-19 15:28:28 -0500

    Add: login functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,7 +6,21 @@ import User from '../models/user.model.js';
 // @route   POST /api/v1/auth/login
 // @access  Public
 export const login = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Hello User' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPasswords(password))) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid Credentials');
+  }
 });
 
 // @desc    Register User / set token
@@ -45,5 +59,10 @@ export const register = asyncHandler(async (req, res) => {
 // @route   POST /api/v1/auth/logout
 // @access  Public
 export const logout = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Goodbye User' });
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: 'User Logged Out' });
 });
